47294fee37a22a6fe0e5212c6c41b9ed313f4f46
Daniela Cortez <dacortez0898@gmail.com>
2019-10-22 22:44:42 -0700

    Aplicación que obtiene el total que se le tiene que cobrar al cliente sobre los servicios que se realizó.

---
+++ BarberShop/BarberShop/BarberShop/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarberShop
+{
+    public partial class MainPage : ContentPage
+    {
+        int cortes = 100;
+        int cejas = 20;
+        int barba = 40;
+        int mascarillas = 50;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void B1_Clicked(object sender, EventArgs e)
+        {
+            int totalCortes = Convert.ToInt32(cortes) * Convert.ToInt32(txt1.Text);
+            int totalCejas = Convert.ToInt32(cejas) * Convert.ToInt32(txt2.Text);
+            int totalBarba = Convert.ToInt32(barba) * Convert.ToInt32(txt3.Text);
+            int totalMascarillas = Convert.ToInt32(mascarillas) * Convert.ToInt32(txt4.Text);
+
+            int totalPago = totalCortes + totalCejas + totalBarba + totalMascarillas;
+
+            txtTotal.Text += totalPago.ToString();
+        }
+    }
+}
